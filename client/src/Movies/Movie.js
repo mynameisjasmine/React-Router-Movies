18b668bd4aa647c38e81f03dec496715087e1f5e
Jasmine <jasminecharles@hotmail.com>
2019-07-23 13:38:50 -0700

    saved list stretch

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import addToSavedList from "../App";
 import axios from 'axios';
 
 const Movie = (props) => {
@@ -21,10 +22,10 @@ const Movie = (props) => {
   },[]);
   
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+   const saveMovie = () => {
+     const addToSavedList = props.addToSavedList;
+    addToSavedList(movie)
+   }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -49,7 +50,7 @@ const Movie = (props) => {
           </div>
         ))}
       </div>
-      <div className="save-button">Save</div>
+      <div className="save-button" onClick={() => saveMovie()}>Save</div>
     </div>
   );
 }
